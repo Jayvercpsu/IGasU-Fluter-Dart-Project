cb092fbcdd24fc15649e818d20bc4678a10c9d67
Jayvercpsu <jayver.cpsu@gmail.com>
2026-07-18 10:11:44 +0800

    updated graddle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         }
         release {
             signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
